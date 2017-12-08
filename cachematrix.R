2be564cc0fe6ffbc0c5feb42ee1490e40b56c6a4
Scott Bihl <sbihl@directionsresearch.com>
2017-12-08 13:32:39 -0500

    Revert "ProgrammingAssignment2"
    This reverts commit 1a055e1469beb48f432e0c513747e477c0b32a60.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ## makeCacheMatrix and cacheSolve return a matrix inversion
 ## result is produced from cached prior result or calculated if no prior exists
 ## 
@@ -34,40 +33,3 @@ cacheSolve <- function(x, ...) {
         x$setinvt(invt)
         invt
 }
-=======
-## makeCacheMatrix and cacheSolve return a matrix inversion
-## result is produced from cached prior result or calculated if no prior exists
-## 
-## simple modification of mean example with matrix invert instead
-
-makeCacheMatrix <- function(x = matrix()) {
-        invt <- NULL
-        set <- function(y) {
-                x <<- y
-                invt <<- NULL
-        }
-        get <- function() x
-        setinvt <- function(inverse) invt <<- inverse
-        getinvt <- function() invt
-        list(set = set, get = get,
-             setinvt = setinvt,
-             getinvt = getinvt)
-}
-
-
-## Invert a square matrix or return a cached calcuation 
-## simple modification of mean example using with solve instead
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        invt <- x$getinvt()
-        if(!is.null(invt)) {
-                message("getting cached data")
-                return(invt)
-        }
-        data <- x$get()
-        invt <- solve(data)
-        x$setinvt(invt)
-        invt
-}
->>>>>>> 939e8803625fa1a7b8de337d1da5ef05b6d6927f
